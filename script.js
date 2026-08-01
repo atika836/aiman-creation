1d3b52b7fcc6f5d86dd8b5a8c0496b6a5ba67b51
atika836 <aatikakhatoon2010@gmail.com>
2026-08-01 19:19:28 +0530

    Code

--- script.js
+++ script.js
@@ -1,56 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { db } from "./firebase.js";
 import {
-  getFirestore,
   collection,
   getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDCVO-BMTMXOsTOTt-7SZuvdjj1PECG72o",
-  authDomain: "aiman-creation-20865.firebaseapp.com",
-  projectId: "aiman-creation-20865",
-  storageBucket: "aiman-creation-20865.firebasestorage.app",
-  messagingSenderId: "639010047446",
-  appId: "1:639010047446:web:b83d01b56ef531b9751077"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 async function loadProducts() {
   const container = document.getElementById("dynamicProducts");
-  if (!container) return;
+  container.innerHTML = "";
 
-  try {
-    const snapshot = await getDocs(collection(db, "products"));
+  const querySnapshot = await getDocs(collection(db, "products"));
 
-    snapshot.forEach((doc) => {
-      const data = doc.data();
+  querySnapshot.forEach((doc) => {
+    const product = doc.data();
 
-      const card = document.createElement("div");
-      card.className = "card";
-
-      card.innerHTML = `
-        <img src="${data.image}">
-        <h3>${data.name}</h3>
-        <p>₹${data.price}</p>
-        <button onclick="addToCart('${data.name}')">Add to Cart</button>
-        <button onclick="buyNow('${data.name}')">Buy Now</button>
-      `;
-
-      container.appendChild(card);
-    });
-  } catch (error) {
-    console.error("Error loading products:", error);
-  }
+    container.innerHTML += `
+      <div class="card">
+        <img src="${product.image}" alt="${product.name}">
+        <h3>${product.name}</h3>
+        <p>₹${product.price}</p>
+        <button>Add to Cart</button>
+        <button>Buy Now</button>
+      </div>
+    `;
+  });
 }
 
-window.addToCart = function (name) {
-  alert(`${name} cart mein add ho gaya`);
-};
-
-window.buyNow = function (name) {
-  alert(`${name} ke liye order kiya jaa raha hai`);
-};
-
-loadProducts()
+loadProducts();
